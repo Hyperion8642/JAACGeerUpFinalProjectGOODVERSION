d5d269b0d790e774de31f478ffa9dba36a7977dd
Amy <54121604+AmyLiu-siyan@users.noreply.github.com>
2019-08-16 10:03:50 -0700

    Update FinanceCalculator.java

--- FinanceCalculator.java
+++ FinanceCalculator.java
@@ -1,23 +1,37 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
+import javax.swing.*;
 
 public class FinanceCalculator {
     private static DecimalFormat df = new DecimalFormat("#.##");
     public static void main (String[] args) {
         System.out.print("Enter your total amount: $");
         Scanner total = new Scanner(System.in);
-        String[] items = {"food? $", "housing? $", "clothing? $", "your automobile? $","appliances? $","services? $",};
+        String[] items = {"food?", "housing?", "clothing?", "your automobile?","appliances?","services?",};
         double totalAmount = total.nextDouble();
         double sum = 0;
+        double [] itemsPrice = new double [6];
 
         for (int i = 0; i < items.length; i++) {
-            System.out.print("How much did you spend on " + items[i]);
+            System.out.print("How much did you spend on " + items[i]+"s");
             Scanner itemInputCost = new Scanner(System.in);
             double itemCost = itemInputCost.nextDouble();
             sum = sum + itemCost;
             double deductions = totalAmount - sum;
+            itemsPrice[i] = itemCost;
             System.out.println("Your leftover balance is $" + df.format(deductions));
         }
+        System.out.println();
+        System.out.print("Items");
+        System.out.print("          Percentage");
+        System.out.println();
+        for(int x=0;x< items.length; x++){
+            double percentage = Math.round(((itemsPrice[x]/sum)*10000));
+            System.out.println(items[x]);
+            System.out.print("                  "+percentage*0.01+"%");
+            System.out.println();
+        }
+
         double balance = totalAmount - sum;
         System.out.println();
         System.out.println("Final Balance: $" + df.format(balance));
@@ -28,3 +42,4 @@ public class FinanceCalculator {
         }
     }
 }
+
